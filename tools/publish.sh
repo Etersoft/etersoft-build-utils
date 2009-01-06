1d4ead506aeda2df1df576c6316cfb2da48a8da3
Vitaly Lipatov <lav@etersoft.ru>
2009-01-06 15:24:38 +0300

    add --tags in publish.sh script (thanks, raorn@)

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 PROJECT=etersoft-build-utils
-git push --all git.eter:packages/$PROJECT.git
-git push --all git.alt:packages/$PROJECT.git
+git push --tags git.eter:packages/$PROJECT.git
+git push --tags git.alt:packages/$PROJECT.git
