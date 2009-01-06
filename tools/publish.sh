208db121a03693eafe9408de779eb1aef6771cc3
Vitaly Lipatov <lav@etersoft.ru>
2009-01-06 22:36:14 +0300

    publish: use --all and --tags

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 PROJECT=etersoft-build-utils
+git push --all git.eter:packages/$PROJECT.git
+git push --all git.alt:packages/$PROJECT.git
 git push --tags git.eter:packages/$PROJECT.git
 git push --tags git.alt:packages/$PROJECT.git
