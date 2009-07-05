0c78c1e5affc28d48632e4c5aa45576e7e07ce14
Vitaly Lipatov <lav@altlinux.ru>
2009-07-05 21:17:04 +0400

    publish.sh: add -f option

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PROJECT=etersoft-build-utils
-git push --all git.eter:packages/$PROJECT.git
-git push --all git.alt:packages/$PROJECT.git
-git push --tags git.eter:packages/$PROJECT.git
-git push --tags git.alt:packages/$PROJECT.git
+git push --all $@ git.eter:packages/$PROJECT.git
+git push --all $@ git.alt:packages/$PROJECT.git
+git push --tags $@ git.eter:packages/$PROJECT.git
+git push --tags $@ git.alt:packages/$PROJECT.git
