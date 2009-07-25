c5538ff8309c6383383c393211eb0c71963d8b81
Vitaly Lipatov <lav@etersoft.ru>
2009-07-25 23:24:51 +0400

    test: improve replacement checking

--- tests/test_repl_find.sh
+++ tests/test_repl_find.sh
@@ -7,15 +7,18 @@ load_mod repl
 #
 check()
 {
-	[ "$2" != "$3" ] && echo "FATAL with '$1': result '$2' do not match with '$3'" || echo "OK for '$1' with '$2'"
+	[ "$2" != "$3" ] && echo "FATAL with '$1': result '$2' do not match with expected '$3'" || echo "OK for '$1' with '$2'"
+}
+
+print_replace()
+{
+	tolocal_anyrepl $1 `print_pkgrepl_list` || TARGETPKGNAME="$1"
+	echo -n "$TARGETPKGNAME"
 }
 
 
 get_pkglist()
 {
-VENDOR=Fedora
-TARGET=rpm
-DISTRVERSION=10
 for i in `print_pkgrepl_list` ; do
 	echo $(basename $i)
 done
@@ -23,19 +26,38 @@ done
 
 get_grplist()
 {
-VENDOR=Fedora
-TARGET=rpm
-DISTRVERSION=10
 for i in `print_grprepl_list` ; do
 	echo $(basename $i)
 done
 }
 
+VENDOR=Fedora
+TARGET=rpm
+DISTRVERSION=10
+
 echo "PkgRepl:"
 get_pkglist
 echo
-check pkgrepl "fedora" `get_pkglist`
+#check pkgrepl "fedora" `get_pkglist`
 
 echo "GrpRepl:"
 get_grplist
 echo
+
+check_repl()
+{
+	check $1 "`print_replace $1`" $2
+}
+
+echo "Correct replacement checking for $VENDOR/$DISTRVERSION (target $TARGET):"
+check_repl rpm-build-compat rpm-build-altlinux-compat
+check_repl libusb-devel libusb-devel
+
+
+VENDOR=Debian
+TARGET=deb
+DISTRVERSION=5.0
+
+echo "Correct replacement checking for $VENDOR/$DISTRVERSION (target $TARGET):"
+check_repl rpm-build-compat rpm-build-altlinux-compat
+check_repl libusb-devel libusb-dev
