74342b0f2abee91c7ab0bab7e055bcbdd1e4c11c
Vitaly Lipatov <lav@etersoft.ru>
2010-02-21 01:29:26 +0300

    introduce drop_args function with test, use it in rpmbsh

---
+++ tests/test_drop_args.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+. `dirname $0`/../share/eterbuild/functions/common
+load_mod git
+
+
+check()
+{
+        [ "$2" != "$3" ] && echo "FATAL with '$1': result '$3' do not match with '$2'" || echo "OK for '$1' with '$2'"
+}
+
+check 1 "-h" "$(drop_args "-h -U" U)"
+check 2 "-h" "$(drop_args " -h -U " U)"
+check 3 "-U" "$(drop_args " -h -U " h)"
+check 4 "-U -i" "$(drop_args " -h -U -i" h)"
+check 5 "" "$(drop_args " -h -U " h U)"
+check 6 "" "$(drop_args " -h -U " U h)"
