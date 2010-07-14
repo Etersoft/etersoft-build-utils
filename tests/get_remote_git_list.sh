7bb1d7cd610140a5e7f8301ec6c7e28dfaf49e36
Vitaly Lipatov <lav@etersoft.ru>
2010-07-14 19:45:36 +0400

    introduce is_one_girar_name

--- tests/get_remote_git_list.sh
+++ tests/get_remote_git_list.sh
@@ -4,3 +4,12 @@
 load_mod git
 
 get_remote_git_list
+
+#REMOTELIST=$(get_remote_git_list)
+#if [ "$(echo $REMOTELIST | cut -d" " -f1)" = "$REMOTELIST" ]  ; then
+#    echo "diag a one name"
+#fi
+
+if is_one_girar_name "$(get_remote_git_list)" ; then
+	echo "diag as one name"
+fi
