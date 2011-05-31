87a2e3c59e8f8baf4547ba0726dd70d08c361500
Vitaly Lipatov <lav@etersoft.ru>
2011-05-31 10:46:34 +0400

    add support for any p? system, do name translation more correctly

--- tests/transf_altdistrversion.sh
+++ tests/transf_altdistrversion.sh
@@ -18,12 +18,17 @@ check_MOD()
 	check $1 $2 `get_altdistr_mod $1`
 }
 
+check_GN()
+{
+	check $1 $2 `get_type_by_git_branch_name $1`
+}
 
 echo "Check get_altdistr_version"
 check_AD M40 4.0
 check_AD M30 3.0
 check_AD M41 4.1
-check_AD M50 5.0
+check_AD M50 p5
+check_AD M51 5.1
 check_AD  SS sisyphus
 
 echo "Check get_altdist_mod"
@@ -32,4 +37,13 @@ check_MOD 3.0 M30
 check_MOD 4.0 M40
 check_MOD 4.1 M41
 check_MOD 5.0 M50
+check_MOD 5.1 M51
+check_MOD  p5 M50
+check_MOD  p6 M60
 check_MOD Sisyphus SS
+check_MOD sisyphuS SS
+
+check_GN M50 M50
+check_GN 5.0 M50
+check_GN p5 M50
+check_GN p7 M70
