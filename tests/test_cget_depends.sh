567ddb118d692b307b75afe7ae1cd502ce88bdc5
Vitaly Lipatov <lav@etersoft.ru>
2012-05-15 21:33:16 +0400

    fix trans_rpmdeps_to_pkgname (requires conversion to package name)

--- tests/test_cget_depends.sh
+++ tests/test_cget_depends.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 # load common functions, compatible with local and installed script
 . `dirname $0`/../share/eterbuild/functions/common
 load_mod alt
 
-testfile=$(echo /var/ftp/pub/Etersoft/RX@Etersoft/testing/CentOS/6/nxclient-*.i586.rpm)
+#testfile=$(echo /var/ftp/pub/Etersoft/RX@Etersoft/testing/CentOS/6/nxclient-*.i586.rpm)
+testfile=$(echo /var/ftp/pub/Etersoft/RX@Etersoft/testing/ALTLinux/Sisyphus/rx-etersoft-*.i586.rpm)
 trans_rpmdeps_to_pkgname $testfile
