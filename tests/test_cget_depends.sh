31646defc5425b128f7fce02f52338353f04064b
Vitaly Lipatov <lav@etersoft.ru>
2012-05-15 16:46:10 +0400

    add trans_rpmdeps_to_pkgname function for print out real package requires

---
+++ tests/test_cget_depends.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# load common functions, compatible with local and installed script
+. `dirname $0`/../share/eterbuild/functions/common
+load_mod alt
+
+testfile=$(echo /var/ftp/pub/Etersoft/RX@Etersoft/testing/CentOS/6/nxclient-*.i586.rpm)
+trans_rpmdeps_to_pkgname $testfile
