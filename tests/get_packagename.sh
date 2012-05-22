052ae8e2a4589e3fc6f7adde19f3d925816e6e16
Vitaly Lipatov <lav@etersoft.ru>
2012-05-22 21:03:45 +0400

    major rewrite get_pkgname_from_filename

--- tests/get_packagename.sh
+++ tests/get_packagename.sh
@@ -19,3 +19,5 @@ check_pkg pkg-source-less-1.0.spec pkg-source-less
 check_pkg pkg123-1.0.spec pkg123
 check_pkg pkg123[_-]1.0.spec pkg123
 check_pkg pkg*.spec pkg
+check_pkg libpq5.2-9.0eter-9.0.4-alt14.i586.rpm libpq5.2-9.0eter
+check_pkg postgre-etersoft9.0_9.0.4-eter14ubuntu_i386.deb postgre-etersoft9.0
