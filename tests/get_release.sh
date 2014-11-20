f91e6d02c3e905012bed31cadac3d731f347e402
Vitaly Lipatov <lav@etersoft.ru>
2014-11-20 19:22:57 +0300

    fix get_numpartrelease(), add test for it

--- tests/get_release.sh
+++ tests/get_release.sh
@@ -68,29 +68,42 @@ check get_numrelease 3.r3003.1 `get_numrelease`
 TESTREL=alt4.r3003.2
 check get_txtrelease alt `get_txtrelease`
 
+TESTREL=alt51
+check get_numpartrelease 51 `get_numpartrelease $TESTREL`
+
+TESTREL=alt5.2
+check get_numpartrelease 5 `get_numpartrelease $TESTREL`
+
+TESTREL=alt3.r3003.1
+check get_numpartrelease 3 `get_numpartrelease $TESTREL`
+
+TESTREL=eter26.svn724archlinux
+check get_numpartrelease 26 `get_numpartrelease $TESTREL`
+
 BASERELEASE=27.5
 get_mn_fromspec
-check MAJOR  27 $MAJOR
+check MAJOR 27 $MAJOR
 check MINOR 5 $MINOR
 
 BASERELEASE=35
 get_mn_fromspec
 check MAJOR 35 $MAJOR
+# it will more correct if MINOR will null
 check MINOR "35" "$MINOR"
 
 BASERELEASE=27.5.r12002
 get_mn_fromspec
-check MAJOR  27.5 $MAJOR
+check MAJOR 27.5 $MAJOR
 check MINOR r12002 $MINOR
 
 BASERELEASE=27.r12002.1
 get_mn_fromspec
-check MAJOR  27 $MAJOR
+check MAJOR 27 $MAJOR
 check MINOR r12002.1 $MINOR
 
 BASERELEASE=27.5.2
 get_mn_fromspec
-check MAJOR  27.5 $MAJOR
+check MAJOR 27.5 $MAJOR
 check MINOR 2 $MINOR
 
 # from rpmbh:
