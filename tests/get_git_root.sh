0599ff3730991490b49697bdfb06472d822b3623
Vitaly Lipatov <lav@etersoft.ru>
2014-11-25 22:23:09 +0300

    add get_repo_name() and use it

--- tests/get_git_root.sh
+++ tests/get_git_root.sh
@@ -11,3 +11,12 @@ is_gear && echo "IS GEAR" || echo "IS NOT GEAR"
 echo "test gear for root_git_dir"
 is_gear $(get_root_git_dir) && echo "IS GEAR" || echo "IS NOT GEAR"
 
+echo
+echo -n "get_repo_name: "
+get_repo_name
+
+echo -n "get_gear_name: "
+get_gear_name
+
+echo -n "get_remote_repo_name: "
+get_remote_repo_name
