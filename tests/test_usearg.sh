e2b3a85f8f9f681f575c31f4300f10ac84ebde03
Vitaly Lipatov <lav@etersoft.ru>
2014-12-12 21:47:03 +0300

    improve usearg

--- tests/test_usearg.sh
+++ tests/test_usearg.sh
@@ -8,8 +8,10 @@ check()
 	[ "$2" != "$3" ] && echo "FATAL with '$1': result '$2' do not match with '$3'" || echo "OK for '$1' with '$2'"
 }
 
-echo "$(usearg -f TEST)"
-echo "$(usearg -f TEST MORE)"
-echo "$(usearg -f $TEST)"
+check "-f TEST" "$(usearg -f TEST)" "-f TEST"
+check "-f 'TEST MORE'" "$(usearg -f 'TEST MORE')" "-f TEST MORE"
+check "-f 'TEST MORE'" "$(usearg -f TEST MORE)" "-f TEST"
+check "-f 'TEST TEST'" "$(usearg -f TEST TEST)" ""
+check "TEST=$TEST-f \$TEST" "$(usearg -f $TEST)" ""
 TEST=124
-echo "$(usearg -f $TEST)"
+check "TEST=$TEST -f \$TEST" "$(usearg -f $TEST)" "-f 124"
