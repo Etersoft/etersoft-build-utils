c782d0b99f91b0de6d75e71f793a749f648b237e
Vitaly Lipatov <lav@etersoft.ru>
2015-08-23 17:52:28 +0300

    alias: drop obsoleted alias

--- etc/bashrc.d/etersoft-build-utils.sh
+++ etc/bashrc.d/etersoft-build-utils.sh
@@ -1,7 +1,1 @@
-
-alias apti='sudo apt-get install'
-alias apts='apt-cache search'
-alias apto='apt-cache show'
-alias aptw='apt-cache whatdepends'
-
 alias finds='find -type f -print0 | xargs -r0 grep -in'
