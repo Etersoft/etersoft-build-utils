950f0efbf4a31fa4cddff93c008499ea04ca8ac2
Vitaly Lipatov <lav@etersoft.ru>
2017-07-22 22:45:17 +0300

    improve test for get_tardir_from_rules and fix improve get_tardir_from_rules

--- tests/get_tarballname.sh
+++ tests/get_tarballname.sh
@@ -23,10 +23,15 @@ Get version test
 EOF
 }
 
+check()
+{
+	[ "$2" != "$3" ] && echo "FATAL with '$1': result '$2' do not match with '$3'" || echo "OK for '$1' with '$2'"
+}
+
 check_get_version()
 {
-RES=$1
-RESGET=`get_tarballname $SPEC`
+	RES=$1
+	RESGET=`get_tarballname $SPEC`
 	[ "$RES" != "$RESGET" ] && echo "FATAL with 'get_tarballname': result '$RES' do not match with '$RESGET'" || echo "OK for 'get_tarballname' with '$RESGET'"
 }
 
@@ -55,19 +60,36 @@ echo "Source path $SOURCEPATH: "
 gen_spec
 get_etersoft_srpm_path $SPEC
 
-echo "TODO"
 subst "s|Source:.*|Source: http://fp.ru/python-larch_1.20131130.orig.tar.gz|g" $SPEC
 check_get_version python-larch
+subst "s|Source:.*|Source0: http://fp.ru/python-larch_1.20131130.orig.tar.gz|g" $SPEC
+check_get_version python-larch
 
-rm -f $SPEC
-
-[ -s $SPEC ] || SPEC="$1"
+[ -n "$1" ] && SPEC="$1"
 
 build_rpms_name $SPEC
 
-echo "Tarball from rules"
+#echo "Tarball from rules"
 #get_tardir_from_rules tar wine-staging-2.4.0.tar || echo "No tarball"
-get_tardir_from_rules tar || echo "No tarball"
-get_tardir_from_rules tar.gz || echo "No tarball"
+#get_tardir_from_rules tar || echo "No tarball"
+#get_tardir_from_rules tar.gz || echo "No tarball"
+
+echo "Test ostree"
+# TODO: fake .gear
+cd /home/lav/Projects/git-alt/containers/ostree
+SPEC=ostree.spec
+build_rpms_name $SPEC
+
+check_tarball()
+{
+    local TARBALL="$1"
+    local TARDIR=$(get_tardir_from_rules "$(get_ext $TARBALL)" $(basename "$TARBALL"))
+    check "$1" "$(basename $TARDIR)" "$2"
+}
+check_tarball ostree-2017.8.tar ostree
+check_tarball libglnx.tar libglnx
+check_tarball bsdiff.tar bsdiff
+
+# TODO: в rules может задаваться другой name архива
 
 echo "DONE"
