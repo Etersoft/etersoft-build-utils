c44acae5dc400b552e5ffa85fccf1e4f427979ec
REAL <real at altlinux.org>
2018-06-09 14:16:06 +0300

    fix -n output in showcmd output

--- tests/test_docmd.sh
+++ tests/test_docmd.sh
@@ -8,6 +8,12 @@ check()
 	[ "$2" != "$3" ] && echo "FATAL with '$1': result '$2' do not match with '$3'" || echo "OK for '$1' with '$2'"
 }
 
+check_docmd()
+{
+	check "$1" " \$ $1" "$(docmd $1 | head -n1)"
+}
+
+
 func()
 {
 docmd ls $@
@@ -15,5 +21,10 @@ docmd ls "$@"
 ls "$@"
 }
 
-func 1 "1 2"
-func 1 2 3
+#func 1 "1 2"
+#func 1 2 3
+
+TERMOUTPUT=
+
+check_docmd "true false"
+check_docmd "true -n"
