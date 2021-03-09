b4329629a937e87700114d5cbbc42a913d6a8647
Vitaly Lipatov <lav@etersoft.ru>
2021-03-10 01:19:32 +0300

    check_reqs.sh: add --detail

--- check_reqs.sh
+++ check_reqs.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+
+
+if [ "$1" = "--detail" ] ; then
+    for i in bin/* share/eterbuild/functions/* ; do
+        echo
+        echo "==== $i:"
+        /usr/lib/rpm/shell.req $i
+    done
+    exit 0
+fi
+
+
 /usr/lib/rpm/shell.req bin/* share/eterbuild/functions/* | sort -u | tee ./check_et.log
 git diff ./check_et.log
